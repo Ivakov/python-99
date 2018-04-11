99e7acc74ff27afad6fe509b2ea452223fc03bd7
Ivakov <k.iwadate@atomitech.jp>
2018-04-11 19:59:29 +0900

    P07の解答

--- P07/main.py
+++ P07/main.py
@@ -2,7 +2,8 @@ def flatten(x):
     ret=[]
     for X in (x):
         if (isinstance(X, list)):
-            ret = ret + X
+            ret = ret + flatten(X)
         else:
             ret.append(X)
     return ret
+
