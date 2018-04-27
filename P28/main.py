1821da86fc30e0ac730f5f44143c38f63c16e992
Ivakov <k.iwadate@atomitech.jp>
2018-04-27 16:37:38 +0900

    P28の解答

--- P28/main.py
+++ P28/main.py
@@ -7,16 +7,16 @@ def lfsort(x):
     ret=[[x[0]]]
     for i in range(1,len(x)):
         is_added = False
-        for X in ret:
-            if len(X[-1]) == len(x[i]):
-                X.append(x[i])
+        for j in ret:
+            if len(j[-1]) == len(x[i]):
+                j.append(x[i])
                 is_added = True
         if is_added == False:
             ret.append([x[i]])
 
     ret.sort(key=len)
-    retA=[]
-    for Y in ret:
-        retA=retA+Y
+    ret_A=[]
+    for k in ret:
+        ret_A=ret_A+k
 
-    return retA
+    return ret_A
