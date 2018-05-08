26a1305106466fa82c7acf776b288eed6676ec8a
Ivakov <k.iwadate@atomitech.jp>
2018-05-08 20:14:40 +0900

    P27の解答

--- P27/main.py
+++ P27/main.py
@@ -1,24 +1,5 @@
 import copy
 
-def group3(elements, groups=[2,3,4], index=0, temp=None):
-    if temp is None:
-        temp = [[] for _ in range(len(groups))]
-
-    ret = []    
-    for i in range(len(groups)):
-        work = copy.deepcopy(temp)
-        work[i] += [elements[index]]
-
-        if len(work[i]) <= groups[i]:
-            if index + 1 < len(elements):
-                ret += group3(elements, groups, index + 1, work)
-            else:
-                ret += [work]
-
-    return ret
-
-
-
 def group(groups, elements, index=0, temp=None):
     if temp is None:
         temp = [[] for _ in range(len(groups))]
@@ -35,3 +16,8 @@ def group(groups, elements, index=0, temp=None):
                 ret += [work]
 
     return ret
+
+
+def group3(elements):
+    return group([2,3,4],elements)
+
