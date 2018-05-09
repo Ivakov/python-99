74136cd97c5267601eeac025f6ee0d945c9a5860
Ivakov <k.iwadate@atomitech.jp>
2018-05-09 20:02:14 +0900

    P33の解答

--- P33/main.py
+++ P33/main.py
@@ -1,20 +1,10 @@
-def is_coprime(x,y):
-    if x >= y:
-        r = x % y
-        if x % y == 0:
-            if y == 1:
-                return True 
-            else:
-                return False
-        else:
-            return is_coprime(r,y)
+import sys
+sys.path.append('../')
 
-    if y >= x:
-        r = y % x
-        if y % x == 0:
-            if x == 1:
-                return True
-            else:
-                return False
-        else:
-            return is_coprime(x,r)
+from P32.main import gcd
+
+def is_coprime(x,y):
+    if gcd(x,y) == 1:
+        return True
+    else:
+        return False
