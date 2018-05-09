60cd213bef73314e29685dc5fd9f8269b9438835
Ivakov <k.iwadate@atomitech.jp>
2018-05-09 13:55:03 +0900

    P31の解答

---
+++ P31/main.py
@@ -0,0 +1,12 @@
+def is_prime(x,index=2):
+    if x < 2:
+        return False
+    
+    else:
+        is_judge = True        
+        for y in range(index,x):
+            if (x % y == 0):
+                return False
+                is_judge = False
+        if is_judge == True:
+            return True 
