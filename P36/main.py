69f712df7c34222728713edc9510ccf5a18544b2
Ivakov <k.iwadate@atomitech.jp>
2018-05-11 20:20:20 +0900

    P36の解答

--- P36/main.py
+++ P36/main.py
@@ -1,14 +1,13 @@
 import sys
 sys.path.append('../')
 
+from P10.main import encode
 from P35.main import prime_factors
 
 def prime_factors_multi(x):
-    ret = []
-    for i in  prime_factors(x):
-        if (ret == [] or ret[-1][0] != i):
-            ret.append([i,1])
-        else:
-            ret[-1][-1] = ret[-1][-1] + 1
- 
-    return ret
+    assigned = prime_factors(x)
+    encoded = encode(assigned)
+    for e in encoded:
+        e.reverse()
+
+    return encoded
