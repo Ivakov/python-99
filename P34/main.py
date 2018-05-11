03d03cb790a4c529659a73cd5e36b1d83611a722
Ivakov <k.iwadate@atomitech.jp>
2018-05-11 17:56:43 +0900

    P34の解答

--- P34/main.py
+++ P34/main.py
@@ -4,9 +4,9 @@ sys.path.append('../')
 from P33.main import is_coprime
 
 def totient_phi(x):
-    a = []
+    count = 0
     for y in range(1,x):
         if is_coprime(x,y):
-            a.append(y)
-
-    return len(a)
+            count = count + 1
+    
+    return count
