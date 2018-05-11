c6c7af7d7ff85c222201289ae70cccf3c326dcbb
Ivakov <k.iwadate@atomitech.jp>
2018-05-11 18:30:59 +0900

    P35の解答

--- P35/main.py
+++ P35/main.py
@@ -4,11 +4,11 @@ sys.path.append('../')
 from P31.main import is_prime
 
 def prime_factors(x,ret=[]):
-    for i in range(int(x+1)):
+    for i in range(x+1):
         if is_prime(i):
-            if x % i == 0:
-                add = ret + ([i])
-                j = x / i
-                return prime_factors(j,add)
+            answer = divmod(x,i)
+            if answer[1] == 0:
+                add = ret + [i]
+                return prime_factors(answer[0],add)
 
     return ret          
