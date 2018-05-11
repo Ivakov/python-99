20408d75e9bf780b9ffc6f70a5fd49b1abee23fd
Ivakov <k.iwadate@atomitech.jp>
2018-05-11 12:25:10 +0900

    P31の解答

--- P31/main.py
+++ P31/main.py
@@ -1,8 +1,9 @@
 def is_prime(x): 
     if x < 2:
         return False
-    else:       
-        for y in range(2,x):
-            if x % y == 0:
-                return False
-        return True 
+       
+    for y in range(2,x):
+        if x % y == 0:
+            return False
+        
+    return True 
