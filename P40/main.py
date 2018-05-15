5125d25a73b1288638aa60199f4f7a116ea42c84
Ivakov <k.iwadate@atomitech.jp>
2018-05-15 16:37:54 +0900

    P40の解答

--- P40/main.py
+++ P40/main.py
@@ -3,9 +3,11 @@ sys.path.append('../')
 
 from P39.main import prime_numbers
 
-def goldbach(x,y=0):
-    for i in prime_numbers(x,y):
-        for j in prime_numbers(x,y):
+def goldbach(x):
+    y = 0
+    for i in prime_numbers(y,x):
+        for j in prime_numbers(i,x):
             if i + j == x:
                 return [i,j]
     return []
+            
