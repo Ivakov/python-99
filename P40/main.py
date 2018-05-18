0f4708884018e2276a6c96c40d6d18d768f7b2bf
Ivakov <k.iwadate@atomitech.jp>
2018-05-18 10:09:11 +0900

    P40の解答

--- P40/main.py
+++ P40/main.py
@@ -7,6 +7,6 @@ def goldbach(x):
     primes = prime_numbers(2,x)
     for i in primes:
         difference = x-i 
-        if difference in primes:
+        if difference in primes and x % 2 == 0:
             return [i,difference]
     return []
