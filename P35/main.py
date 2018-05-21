776621a716702d558676bf4e309d627ffdbe62bd
Ivakov <k.iwadate@atomitech.jp>
2018-05-21 11:20:27 +0900

    P35の解答

--- P35/main.py
+++ P35/main.py
@@ -7,6 +7,10 @@ def prime_factors(x):
     ret = []
     for i in range(x+1):
         if is_prime(i):
-            answers = divmod(x,i)
-            if answers[1] == 0:
-                print(i,answers[0])
+            divide = divmod(x,i)
+            if divide[1] == 0:
+                ret.append(i)
+                while divide[0] % i == 0:
+                    ret.append(i) 
+                    divide = (divide[0] // i, None)
+    return ret
