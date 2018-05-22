2f2514f1e89f8df8e7b992584bdff31e4a200663
Ivakov <k.iwadate@atomitech.jp>
2018-05-22 19:21:42 +0900

    P35の解答

--- P35/main.py
+++ P35/main.py
@@ -1,14 +1,9 @@
-import sys
-sys.path.append('../')
-
-from P31.main import is_prime
-
 def prime_factors(x):
     ret = []
     i = 2
     while i <= x:
         divide = divmod(x,i)
-        if is_prime(i) and divide[1] == 0:
+        if divide[1] == 0:
             ret.append(i)
             x = divide[0]
         else:
