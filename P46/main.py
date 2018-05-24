8d796790ed9d6e6ca9d95ff5676ac31fe1470474
Ivakov <k.iwadate@atomitech.jp>
2018-05-24 14:50:29 +0900

    P46の解答

--- P46/main.py
+++ P46/main.py
@@ -2,19 +2,19 @@ import itertools
 
 
 def AND(x,y):
-    return x and y 
+    return x & y 
 
 def OR(x,y):
-    return x or y 
+    return x | y
 
 def NAND(x,y):
-    return not x or not y
+    return not x & y
 
 def NOR(x,y):
-    return not x and not y
+    return not x | y
 
 def XOR(x,y):
-    return x != y
+    return x ^ y
 
 def IMP(x,y):
     return x == y or y
