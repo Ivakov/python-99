ffb9bf52f1e0013127f364136c79853df69dd06b
Ivakov <k.iwadate@atomitech.jp>
2018-05-24 11:19:33 +0900

    P35の解答

--- P35/main.py
+++ P35/main.py
@@ -2,10 +2,10 @@ def prime_factors(x):
     ret = []
     i = 2
     while i <= x:
-        j,k = divmod(x,i)
-        if k == 0:
+        a,b = divmod(x,i)
+        if b == 0:
             ret.append(i)
-            x = j
+            x = a
         else:
             i = i + 1
     return ret
