16826b5bfee83898d7f27200c531e172f42d2528
Ivakov <k.iwadate@atomitech.jp>
2018-07-11 16:47:44 +0900

    P09の解答

--- P09/main.py
+++ P09/main.py
@@ -1,8 +1,8 @@
 def pack(x):
-    ret=[]
-    for X in (x):
-        if(ret==[] or ret[-1][-1]!=X):
-            ret=ret+[[X]]
+    ret = []
+    for i in x:
+        if ret == [] or ret[-1][-1] != i:
+            ret = ret + [[i]]
         else:
-            ret[-1].append(X)
+            ret[-1].append(i)
     return ret
