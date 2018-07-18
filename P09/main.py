6d2002f64e7c6cb9ad72adad709c238e24eb6ad8
Ivakov <k.iwadate@atomitech.jp>
2018-07-18 17:53:46 +0900

    P09の解答

--- P09/main.py
+++ P09/main.py
@@ -2,7 +2,7 @@ def pack(x):
     ret = []
     for i in x:
         if ret == [] or ret[-1][-1] != i:
-            ret = ret + [[i]]
+            ret.append([i])
         else:
             ret[-1].append(i)
     return ret
