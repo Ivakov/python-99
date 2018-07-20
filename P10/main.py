6414576c2654eb8fdc92c081ebc44a92bbdda093
Ivakov <k.iwadate@atomitech.jp>
2018-07-20 16:30:24 +0900

    P10の解答

--- P10/main.py
+++ P10/main.py
@@ -1,10 +1,8 @@
 def encode(x):
     ret = []
     for i in x:
-        if (ret == [] or ret[-1][-1] != i):
+        if ret == [] or ret[-1][-1] != i:
             ret.append([1,i])
         else:
-            ret[-1][0] = ret[-1][0] + 1
-
+            ret[-1][0] += 1
     return ret
-
