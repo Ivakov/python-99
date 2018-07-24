6404dd89dce83608c0ee80964dc925dc5d719a29
Ivakov <k.iwadate@atomitech.jp>
2018-07-24 09:54:12 +0900

    P13の解答

--- P13/main.py
+++ P13/main.py
@@ -3,8 +3,8 @@ def encode_direct(x):
     for i in x:
         if ret == []:
             ret.append(i)
-        elif(isinstance(ret[-1],list) and ret[-1][-1] == i):
-            ret[-1][0] = ret[-1][0] + 1
+        elif isinstance(ret[-1],list) and ret[-1][-1] == i:
+            ret[-1][0] += 1
         elif ret[-1] == i:
             ret[-1] = [2,i]
         else:
